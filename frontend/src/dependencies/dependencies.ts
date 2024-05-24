e1fd6b6f7e7bfd4e97e6b783a0726c1c4097e50d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-05-24 17:24:05 +0100

    Material UI Icons dependencies installed

--- frontend/src/dependencies/dependencies.ts
+++ frontend/src/dependencies/dependencies.ts
@@ -6,6 +6,10 @@
     npm i @rocketseat/eslint-config -D
   }
 
+  ### - MATERIAL UI ICONS {
+    npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+  }
+
   ### - REACT ROUTER DOM {
     npm i react-router-dom
   }
