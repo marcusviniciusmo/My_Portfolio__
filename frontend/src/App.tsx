6a1b94d558f2dee96c0b353b4a997d22ffcbefbe
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-05-24 17:11:37 +0100

    Profile component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './routes/Router';
-import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
+import { Profile } from './components/Profile';
 import { Menu } from './components/Menu';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
       <Header />
+      <Profile />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
---
+++ frontend/src/components/Profile/index.tsx
@@ -0,0 +1,7 @@
+export function Profile() {
+  return (
+    <>
+      <h1>PROFILE COMPONENT</h1>
+    </>
+  );
+}
