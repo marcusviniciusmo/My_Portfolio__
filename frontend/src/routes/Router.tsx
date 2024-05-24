dec35c1c2845d0375a69444a4ab81df190b35967
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-05-24 16:48:16 +0100

    About component created

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../views/Home';
+import { About } from '../components/About';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
