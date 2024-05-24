9eba71c4f1b20ef47ac53b84f301255a28063da8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-05-24 18:04:27 +0100

    Unfinished: Header component styling

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,7 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import { Home } from '@mui/icons-material';
+import { HeaderContainer } from './styles';
+
 export function Header() {
   return (
-    <>
+    <HeaderContainer>
       <h1>HEADER COMPONENT</h1>
-    </>
+
+      <NavLink to="/">
+        <Home className="headerIcon" />
+      </NavLink>
+
+      <button>Theme Switcher</button>
+    </HeaderContainer>
   );
 }
---
+++ frontend/src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  margin-inline: 8rem;
+  padding-block: 5rem 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
