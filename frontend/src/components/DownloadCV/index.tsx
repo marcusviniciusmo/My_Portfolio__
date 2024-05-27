b30f92693b0127e8d3c431ac6e115a17e07b7fe7
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-05-27 20:09:20 +0100

    Downloading CV by component

--- frontend/src/components/DownloadCV/index.tsx
+++ frontend/src/components/DownloadCV/index.tsx
@@ -2,10 +2,22 @@ import { Download } from '@mui/icons-material';
 import { DownloadCVContainer } from './styles';
 
 export function DownloadCV() {
+  function handleDownload() {
+    const link = document.createElement('a');
+
+    link.href = '/curriculum/CV_Marcus_Oliveira.pdf';
+    link.download = 'CV_Marcus_Oliveira.pdf';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
   return (
     <DownloadCVContainer
       className="backgroundGradientToRight gradientHover gradientHoverToLeft"
       title="Download CV"
+      onClick={handleDownload}
     >
       <Download className="downloadIcon" fontSize="large" />
       Download CV
