b4ad28e1ef44a51c3594136c5e9f6838712d99db
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-05-31 20:36:16 +0100

    Input component created

--- frontend/src/components/Contact/index.tsx
+++ frontend/src/components/Contact/index.tsx
@@ -1,10 +1,16 @@
 import { TitleContentPage } from '../TitleContentPage';
+import { Input } from '../Input';
 
 export function Contact() {
   return (
     <div className="contentContainer">
       <TitleContentPage title="Contact" left="16.8" />
       <h1>CONTACT COMPONENT</h1>
+      <p>Thanks for taking the time to reach out.</p>
+      <Input />
+      <Input />
+      <textarea />
+      <button>Submit</button>
     </div>
   );
 }
---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,7 @@
+export function Input() {
+  return (
+    <>
+      <h1>INPUT COMPONENT</h1>
+    </>
+  );
+}
