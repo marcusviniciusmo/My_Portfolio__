e251a48c5c84b44f708ca4c9360744c96d327c61
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-06-26 06:56:14 +0100

    DefaultLayout (Code Review)

--- frontend/src/layout/DefaultLayout/DefaultLayout.tsx
+++ frontend/src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+
 import { Header } from '../../components/Header';
 import { Profile } from '../../components/Profile';
 import { Menu } from '../../components/Menu';
 import { ContentLayout } from '../ContentLayout';
+
 import { DefaultLayoutContainer } from './styles';
 
 export function DefaultLayout() {
