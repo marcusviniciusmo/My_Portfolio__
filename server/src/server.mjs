dab063d29437e68be707eb423d993cadc5ad626f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-08-16 09:00:44 +0100

    First route returning JSON

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.send('Acessou response!');
+  return response.json([{message: 'Acessou response json!'}]);
 });
 
 app.listen(3333);
