e32e5fc87e8870f839f12e3539803c13b841606e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-10-13 04:30:32 -0300

    Added transaction to Create certificate types route

--- server/src/repositories/CertificateTypes/index.ts
+++ server/src/repositories/CertificateTypes/index.ts
@@ -5,35 +5,37 @@ import { ThrowConflictException, ThrowRepositoryException } from "../../utils/Fu
 export const CreateCertificateTypesRepository = async (route: string) => {
   const certificateTypesToInsert = GetCertificateTypesToInsert();
 
-  try {
-    const existingCertificateTypes = await GetCertificateTypesRepository(route);
-
-    const existingCertifcateTypesDescription = new Set(
-      existingCertificateTypes?.map(
-        (type) => type.typeDescription.toLowerCase().trim()
-      )
-    );
-
-    const typesToInsert = certificateTypesToInsert.filter(
-      (typeToInsert) =>!existingCertifcateTypesDescription.has(
-        typeToInsert.typeDescription.toLowerCase().trim()
-      )
-    );
-
-    if (typesToInsert.length === 0) {
-      ThrowConflictException(route);
+  return await prisma.$transaction(async (prisma) => {
+    try {
+      const existingCertificateTypes = await GetCertificateTypesRepository(route);
+  
+      const existingCertifcateTypesDescription = new Set(
+        existingCertificateTypes?.map(
+          (type) => type.typeDescription.toLowerCase().trim()
+        )
+      );
+  
+      const typesToInsert = certificateTypesToInsert.filter(
+        (typeToInsert) =>!existingCertifcateTypesDescription.has(
+          typeToInsert.typeDescription.toLowerCase().trim()
+        )
+      );
+  
+      if (typesToInsert.length === 0) {
+        ThrowConflictException(route);
+      };
+  
+      const insertedCertificateTypes = Promise.all(
+        typesToInsert.map((typeToInsert) => prisma.certificateTypes.create({
+          data: typeToInsert
+        }))
+      );
+  
+      return insertedCertificateTypes;
+    } catch (error) {
+      ThrowRepositoryException(route, undefined, error);
     };
-
-    const insertedCertificateTypes = Promise.all(
-      typesToInsert.map((typeToInsert) => prisma.certificateTypes.create({
-        data: typeToInsert
-      }))
-    );
-
-    return insertedCertificateTypes;
-  } catch (error) {
-    ThrowRepositoryException(route, undefined, error);
-  };
+  });
 };
 
 export const GetCertificateTypesRepository = async (route: string) => {
